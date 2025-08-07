d58ba97a9d666ebb5adcea2cb1fe7948c80bfd45
Aman4198 <aman04011998@gmail.com>
2025-08-07 17:20:06 +0530

    commit 6

---
+++ src/seleniumpackage/Hello6.java
@@ -0,0 +1,11 @@
+package seleniumpackage;
+
+public class Hello6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world 6");
+	}
+
+}
