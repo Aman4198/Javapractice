309917ed8c249d0f1af52eb6e394b2b7f117e43d
Aman4198 <aman04011998@gmail.com>
2025-08-07 17:34:49 +0530

    today last commit

--- src/seleniumpackage/Hello7.java
+++ src/seleniumpackage/Hello7.java
@@ -1,11 +1,11 @@
 package seleniumpackage;
 
-public class Hello6 {
+public class Hello7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello world 6");
+		//System.out.println("hello world 6");
 		System.out.println("hello world 7");
 	}
 
